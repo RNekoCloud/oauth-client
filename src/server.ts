c2be8ac00b12deeb71c2613a0c53e963d8143b93
RageNeko26 <rneko2006@gmail.com>
2023-09-08 00:06:19 +0700

    feat: adding explicit response

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import { Express, Request, Response } from "express"
 import * as express from "express"
 
+const path = require("path")
+
 const app: Express = express()
 const port: number = 3000
 
 app.get("/api/v1", (req: Request, res: Response) => {
 	res.json({
-		message: "API Version 1.0.0"
+		message: "API Version 1.0.0",
+		success: true,
+		status: 200
 	})
 })
 
