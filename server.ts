e859cdbbd89c9d2a417daa96a31bf121a5dd0982
RageNeko26 <rneko2006@gmail.com>
2023-09-08 11:03:13 +0700

    feat: Serving React App

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
 import * as express from "express" 
 import { Express, Request, Response } from "express"
 
+const paht = require("path")
+
 const app: Express = express()
 const port: number = 3000
+
+app.use("/assets", express.static(path.join(__dirname, "dist")))
 
 // Test route
 app.get("/api", (_, res: Response) => {
@@ -13,9 +17,11 @@ app.get("/api", (_, res: Response) => {
 
 app.get("/verify/:token", (req: Request, res: Response) => {
 	const token = req.params.token
-	res.json({
-		message: token,
-	})
+	// Serving React Route
+})
+
+app.get("*", (_, res: Response) => {
+	res.sendFile(path.join(__dirname "/dist/index.html"))
 })
 
 app.listen(port, (): void => {
